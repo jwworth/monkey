7370f8862dbcf3eeed5e8b1d3722f7a5e7aa48af
Jake Worth <jake@jakeworth.com>
2018-09-23 11:34:54 -0500

    Parse return statements

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -40,41 +40,41 @@ func TestLetStatements(t *testing.T) {
 	}
 }
 
-// func TestReturnStatements(t *testing.T) {
-// 	tests := []struct {
-// 		input         string
-// 		expectedValue interface{}
-// 	}{
-// 		{"return 5;", 5},
-// 		{"return true;", true},
-// 		{"return foobar;", "foobar"},
-// 	}
+func TestReturnStatements(t *testing.T) {
+	tests := []struct {
+		input         string
+		expectedValue interface{}
+	}{
+		{"return 5;", 5},
+		{"return true;", true},
+		{"return foobar;", "foobar"},
+	}
 
-// 	for _, tt := range tests {
-// 		l := lexer.New(tt.input)
-// 		p := New(l)
-// 		program := p.ParseProgram()
-// 		checkParserErrors(t, p)
+	for _, tt := range tests {
+		l := lexer.New(tt.input)
+		p := New(l)
+		program := p.ParseProgram()
+		checkParserErrors(t, p)
 
-// 		if len(program.Statements) != 1 {
-// 			t.Fatalf("program.Statements does not contain 1 statements. got=%d",
-// 				len(program.Statements))
-// 		}
+		if len(program.Statements) != 1 {
+			t.Fatalf("program.Statements does not contain 1 statements. got=%d",
+				len(program.Statements))
+		}
 
-// 		stmt := program.Statements[0]
-// 		returnStmt, ok := stmt.(*ast.ReturnStatement)
-// 		if !ok {
-// 			t.Fatalf("stmt not *ast.returnStatement. got=%T", stmt)
-// 		}
-// 		if returnStmt.TokenLiteral() != "return" {
-// 			t.Fatalf("returnStmt.TokenLiteral not 'return', got %q",
-// 				returnStmt.TokenLiteral())
-// 		}
-// 		if testLiteralExpression(t, returnStmt.ReturnValue, tt.expectedValue) {
-// 			return
-// 		}
-// 	}
-// }
+		stmt := program.Statements[0]
+		returnStmt, ok := stmt.(*ast.ReturnStatement)
+		if !ok {
+			t.Fatalf("stmt not *ast.returnStatement. got=%T", stmt)
+		}
+		if returnStmt.TokenLiteral() != "return" {
+			t.Fatalf("returnStmt.TokenLiteral not 'return', got %q",
+				returnStmt.TokenLiteral())
+		}
+		// if testLiteralExpression(t, returnStmt.ReturnValue, tt.expectedValue) {
+		// 	return
+		// }
+	}
+}
 
 // func TestIdentifierExpression(t *testing.T) {
 // 	input := "foobar;"
