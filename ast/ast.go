7eae075b468121b8888bf29650f66f5952ecfcba
Jake Worth <jake@jakeworth.com>
2018-10-15 07:34:42 -0500

    Define call expressions

--- ast/ast.go
+++ ast/ast.go
@@ -250,3 +250,29 @@ func (fl *FunctionLiteral) String() string {
 
 	return out.String()
 }
+
+// Define call expressions
+type CallExpression struct {
+	Token     token.Token // The '(' token
+	Function  Expression  // Identifer or function literal
+	Arguments []Expression
+}
+
+// Helper methods for call expressions
+func (ce *CallExpression) expressionNode()      {}
+func (ce *CallExpression) TokenLiteral() string { return ce.Token.Literal }
+func (ce *CallExpression) String() string {
+	var out bytes.Buffer
+	args := []string{}
+
+	for _, a := range ce.Arguments {
+		args = append(args, a.String())
+	}
+
+	out.WriteString(ce.Function.String())
+	out.WriteString("(")
+	out.WriteString(strings.Join(args, ", "))
+	out.WriteString(")")
+
+	return out.String()
+}
