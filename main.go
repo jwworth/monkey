1eb27101c3c192d889e7b9e2e148ba83928f4100
Jake Worth <jake@jakeworth.com>
2018-10-16 13:09:57 -0500

    Terminate a sentence

--- main.go
+++ main.go
@@ -13,6 +13,6 @@ func main() {
 		panic(err)
 	}
 	fmt.Printf("Hello %s! This is the Monkey programming language!\n", user.Username)
-	fmt.Printf("Feel free to type in commands\n")
+	fmt.Printf("Feel free to type in commands.\n")
 	repl.Start(os.Stdin, os.Stdout)
 }
