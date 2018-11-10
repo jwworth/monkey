25ce350c24cb87d1fffd6d8998756d4d9ed5ab2e
Jake Worth <jake@jakeworth.com>
2018-11-10 09:22:12 -0600

    Allow evaluator to handle strings

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -97,6 +97,9 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 		}
 
 		return applyFunction(function, args)
+
+	case *ast.StringLiteral:
+		return &object.String{Value: node.Value}
 	}
 
 	return nil
