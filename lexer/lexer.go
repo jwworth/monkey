db554d277ce1a5c8b7bafe6ea45e3c896d35f88d
Jake Worth <jake@jakeworth.com>
2018-11-10 09:09:20 -0600

    Add lexer support for strings

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -21,6 +21,10 @@ func (l *Lexer) NextToken() token.Token {
 	l.skipWhitespace()
 
 	switch l.ch {
+
+	case '"':
+		tok.Type = token.STRING
+		tok.Literal = l.readString()
 	case '=':
 		if l.peekChar() == '=' {
 			ch := l.ch
@@ -137,4 +141,18 @@ func (l *Lexer) peekChar() byte {
 	} else {
 		return l.input[l.readPosition]
 	}
+}
+
+func (l *Lexer) readString() string {
+	position := l.position + 1
+
+	for {
+		l.readChar()
+
+		if l.ch == '"' || l.ch == 0 {
+			break
+		}
+	}
+
+	return l.input[position:l.position]
 }
--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -22,7 +22,10 @@ func TestNextToken(t *testing.T) {
 		return false;
 	}
 	
-	10 == 10; 10 != 9;
+	10 == 10;
+	10 != 9;
+	"foobar"
+	"foo bar"
 	`
 
 	tests := []struct {
@@ -102,6 +105,8 @@ func TestNextToken(t *testing.T) {
 		{token.NOT_EQ, "!="},
 		{token.INT, "9"},
 		{token.SEMICOLON, ";"},
+		{token.STRING, "foobar"},
+		{token.STRING, "foo bar"},
 		{token.EOF, ""},
 	}
 
